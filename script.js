88cca9ac5f74544d2da60e62fdb25be71ae605a6
Sourabh Bansotiya <77042105+sourabh-bansotiya@users.noreply.github.com>
2024-04-01 23:24:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 //your JS code here. If required.
-const d = new Date();
-d.setHours(24);
-document.getElementById("demo").innerHTML = d;
+function dateTime() {
+	let currentDate = new Date();
+
+	let hours = currentDate.getHours();
+	let mins = currentDate.getMinutes();
+	let secs = currentDate.getSeconds();
+
+	mins = (mins < 10 ? "0" : "") + mins;
+	secs = (secs < 10 ? "0" : "") +secs;
+
+	let day = currentDate.getDate();
+	let month = currentDate.getMonth() + 1;
+	let year = currentDate.getFullYear();
+
+	let currentTime = hours + ":" + mins + ":" + secs;
+	let currentDateStr = day + "/" + month +"/" + year;
+
+	alert("Time: " + currentTime + "\nDate: " + currentDateStr);
+
+	document.getElementById("timer").innerHTML = "Time: " + currentTime + "| Date: " + currentDateStr;
+}
+setInterval(displayDateTime , 1000);
